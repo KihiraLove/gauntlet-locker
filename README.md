87b76507393e0493eb7daeb9a73e256853489e4a
Kihira <d.kertesz16@gmail.com>
2026-01-23 08:38:05 +0100

    init

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# Example
-An example greeter plugin
+# Gauntlet Locker
+Locks the gauntlet portal to be unaccessible.  
+This plugin was requested by Only_Trails for his upcoming series.
--- src/main/java/com/gauntletlocker/GauntletLockerPlugin.java
+++ src/main/java/com/gauntletlocker/GauntletLockerPlugin.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.gauntletlocker;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
@@ -14,15 +14,15 @@ import net.runelite.client.plugins.PluginDescriptor;
 
 @Slf4j
 @PluginDescriptor(
-	name = "Example"
+	name = "Gauntlet Locker"
 )
-public class ExamplePlugin extends Plugin
+public class GauntletLockerPlugin extends Plugin
 {
 	@Inject
 	private Client client;
 
 	@Inject
-	private ExampleConfig config;
+	private GauntletLockerConfig config;
 
 	@Override
 	protected void startUp() throws Exception
@@ -46,8 +46,8 @@ public class ExamplePlugin extends Plugin
 	}
 
 	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
+	GauntletLockerConfig provideConfig(ConfigManager configManager)
 	{
-		return configManager.getConfig(ExampleConfig.class);
+		return configManager.getConfig(GauntletLockerConfig.class);
 	}
 }
