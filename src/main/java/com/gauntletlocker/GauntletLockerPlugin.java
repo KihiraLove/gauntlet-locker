eadaec1c001b83ab2cb60a5fecbac43d9553e38f
Kihira <d.kertesz16@gmail.com>
2026-09-05 10:06:04 +0200

    Implement locked portal Death sequence

--- src/main/java/com/gauntletlocker/GauntletLockerPlugin.java
+++ src/main/java/com/gauntletlocker/GauntletLockerPlugin.java
@@ -1,28 +1,35 @@
 package com.gauntletlocker;
 
 import com.google.inject.Inject;
+import com.google.inject.Provides;
 import java.util.HashSet;
 import java.util.Set;
-
-import com.google.inject.Provides;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.ChatMessageType;
 import net.runelite.api.Client;
 import net.runelite.api.GameObject;
+import net.runelite.api.GameState;
+import net.runelite.api.Model;
+import net.runelite.api.ModelData;
+import net.runelite.api.NPCComposition;
 import net.runelite.api.Player;
+import net.runelite.api.Point;
+import net.runelite.api.RuneLiteObject;
+import net.runelite.api.Scene;
+import net.runelite.api.Tile;
+import net.runelite.api.AnimationController;
+import net.runelite.api.coords.LocalPoint;
+import net.runelite.api.coords.WorldPoint;
 import net.runelite.api.events.ClientTick;
 import net.runelite.api.events.GameObjectDespawned;
 import net.runelite.api.events.GameObjectSpawned;
-import net.runelite.api.events.GameTick;
+import net.runelite.api.events.GameStateChanged;
 import net.runelite.api.events.MenuEntryAdded;
 import net.runelite.api.events.MenuOptionClicked;
 import net.runelite.client.callback.ClientThread;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
-import net.runelite.client.game.chatbox.ChatboxPanelManager;
-import net.runelite.client.input.KeyManager;
-import net.runelite.client.input.MouseManager;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 import net.runelite.client.ui.overlay.OverlayManager;
@@ -32,43 +39,40 @@ import net.runelite.client.util.Text;
 @PluginDescriptor(name = "Gauntlet Locker")
 public class GauntletLockerPlugin extends Plugin
 {
-	@Inject private Client client;
-	@Inject private ClientThread clientThread;
-
-	@Inject private OverlayManager overlayManager;
-	@Inject private GauntletPortalOverlay overlay;
-
-	@Inject private ChatboxPanelManager chatboxPanelManager;
-	@Inject private KeyManager keyManager;
-	@Inject private MouseManager mouseManager;
-	@Inject private GauntletLockerConfig config;
-	@Inject private ConfigManager configManager;
-
-	// Portal
 	static final int PORTAL_OBJECT_ID = 36081;
 	static final String PORTAL_NAME = "Gauntlet Portal";
 
-	// Sequence
+	static final int HALLOWEEN_DEATH_NPC_ID = 5567;
+	static final int DEATH_ATTACK_ANIMATION = 440;
+	static final int PLAYER_STUN_ANIMATION = 881;
+	static final int STUN_SOUND_EFFECT = 2727;
+	static final String DEATH_OVERHEAD_TEXT = "I told you this was off limits.";
 
-	private static final int SPOTANIM_KEY = 1;
+	static final int RED_CLICK_CYCLES = 20;
 
-
-	// Overhead duration in client cycles (20ms)
-	private static final int OVERHEAD_CYCLES = 100;
-
-	// Dialogue (your values)
-	private static final int REAPER_NPC_ID = 9855;
-	private static final int REAPER_CHATHEAD_SEQ = 563;
-
-	private static final String REAPER_NAME = "Grim Reaper";
-	private static final String REAPER_DIALOGUE = "you're not welcome here";
-
-	// Menu recolor
 	private static final String GREY_OPEN = "<col=808080>";
 	private static final String GREY_CLOSE = "</col>";
 
-	private enum Phase { IDLE, WAIT_ANIM, WAIT_DIALOGUE, DIALOGUE_OPEN }
+	@Inject private Client client;
+	@Inject private ClientThread clientThread;
+	@Inject private OverlayManager overlayManager;
+	@Inject private GauntletPortalOverlay overlay;
+	@Inject private GauntletLockerConfig config;
 
+	@Getter
+	private final Set<GameObject> portals = new HashSet<>();
+
+	@Getter
+	private RuneLiteObject deathObject;
+
+	@Getter
+	private String deathOverheadText;
+
+	@Getter
+	private Point redClickPoint;
+
+	@Getter
+	private int redClickStartCycle = -1;
 
 	@Provides
 	GauntletLockerConfig provideConfig(ConfigManager configManager)
@@ -76,24 +80,12 @@ public class GauntletLockerPlugin extends Plugin
 		return configManager.getConfig(GauntletLockerConfig.class);
 	}
 
-	@Getter
-	private final Set<GameObject> portals = new HashSet<>();
-
-	private Phase phase = Phase.IDLE;
-
-	private boolean animSeen = false;
-	private int animStartCycle = 0;
-	private int dialogueOpenCycle = -1;
-
-	private GrimReaperDialogueInput reaperDialogue;
-
 	@Override
 	protected void startUp()
 	{
 		overlayManager.add(overlay);
-		portals.clear();
-		phase = Phase.IDLE;
-		dialogueOpenCycle = -1;
+		resetSequence();
+		clientThread.invoke(this::refreshPortals);
 	}
 
 	@Override
@@ -101,254 +93,333 @@ public class GauntletLockerPlugin extends Plugin
 	{
 		overlayManager.remove(overlay);
 		portals.clear();
-		phase = Phase.IDLE;
-		dialogueOpenCycle = -1;
-
-		closeReaperDialogue();
-	}
-
-	// Track portals for overlay
-	@Subscribe
-	public void onGameObjectSpawned(GameObjectSpawned e)
-	{
-		GameObject obj = e.getGameObject();
-		if (obj != null && obj.getId() == PORTAL_OBJECT_ID)
-		{
-			portals.add(obj);
-		}
+		resetSequence();
 	}
 
 	@Subscribe
-	public void onGameObjectDespawned(GameObjectDespawned e)
+	public void onGameStateChanged(GameStateChanged event)
 	{
-		GameObject obj = e.getGameObject();
-		if (obj != null && obj.getId() == PORTAL_OBJECT_ID)
+		if (event.getGameState() == GameState.LOGGED_IN)
 		{
-			portals.remove(obj);
+			refreshPortals();
+		}
+		else if (event.getGameState() == GameState.LOGIN_SCREEN
+			|| event.getGameState() == GameState.HOPPING)
+		{
+			portals.clear();
+			resetSequence();
 		}
 	}
 
-	// Grey the "Enter" menu entry
 	@Subscribe
-	public void onMenuEntryAdded(MenuEntryAdded e)
+	public void onGameObjectSpawned(GameObjectSpawned event)
 	{
-		if (e.getIdentifier() != PORTAL_OBJECT_ID)
+		GameObject object = event.getGameObject();
+		if (object != null && object.getId() == PORTAL_OBJECT_ID)
 		{
-			return;
-		}
-
-		final String opt = Text.removeTags(e.getOption());
-		final String tgt = Text.removeTags(e.getTarget());
-
-		final boolean isEnter = opt.equalsIgnoreCase("Enter") || opt.equalsIgnoreCase("Enter " + PORTAL_NAME);
-		final boolean isPortal = (tgt != null && tgt.toLowerCase().contains(PORTAL_NAME.toLowerCase()))
-				|| opt.toLowerCase().contains(PORTAL_NAME.toLowerCase());
-
-		if (!isEnter || !isPortal)
-		{
-			return;
-		}
-
-		final var me = e.getMenuEntry();
-		me.setOption(GREY_OPEN + opt + GREY_CLOSE);
-		if (tgt != null && !tgt.isEmpty())
-		{
-			me.setTarget(GREY_OPEN + tgt + GREY_CLOSE);
+			portals.add(object);
 		}
 	}
 
-	// Intercept clicks
 	@Subscribe
-	public void onMenuOptionClicked(MenuOptionClicked e)
+	public void onGameObjectDespawned(GameObjectDespawned event)
 	{
-		if (e.getId() != PORTAL_OBJECT_ID)
+		GameObject object = event.getGameObject();
+		if (object != null && object.getId() == PORTAL_OBJECT_ID)
+		{
+			portals.remove(object);
+		}
+	}
+
+	@Subscribe
+	public void onMenuEntryAdded(MenuEntryAdded event)
+	{
+		if (event.getIdentifier() != PORTAL_OBJECT_ID)
 		{
 			return;
 		}
 
-		final String opt = Text.removeTags(e.getMenuOption());
-		final String tgt = Text.removeTags(e.getMenuTarget());
+		String option = Text.removeTags(event.getOption());
+		String target = Text.removeTags(event.getTarget());
 
-		final boolean isPortal = (tgt != null && tgt.toLowerCase().contains(PORTAL_NAME.toLowerCase()))
-				|| opt.toLowerCase().contains(PORTAL_NAME.toLowerCase());
-
-		if (!isPortal)
+		if (!isEnterOption(option) || !isPortal(option, target))
 		{
 			return;
 		}
 
-		if (opt.equalsIgnoreCase("Enter") || opt.equalsIgnoreCase("Enter " + PORTAL_NAME))
+		event.getMenuEntry().setOption(GREY_OPEN + option + GREY_CLOSE);
+		if (target != null && !target.isEmpty())
 		{
-			e.consume();
-			startSequence();
+			event.getMenuEntry().setTarget(GREY_OPEN + target + GREY_CLOSE);
+		}
+	}
+
+	@Subscribe
+	public void onMenuOptionClicked(MenuOptionClicked event)
+	{
+		if (event.getId() != PORTAL_OBJECT_ID)
+		{
 			return;
 		}
 
-		if (opt.equalsIgnoreCase("Examine"))
+		String option = Text.removeTags(event.getMenuOption());
+		String target = Text.removeTags(event.getMenuTarget());
+		if (!isPortal(option, target))
 		{
-			e.consume();
+			return;
+		}
+
+		if (isEnterOption(option))
+		{
+			event.consume();
+			recordRedClick();
+			startDeathSequence();
+			return;
+		}
+
+		if (option.equalsIgnoreCase("Examine"))
+		{
+			event.consume();
 			client.addChatMessage(ChatMessageType.OBJECT_EXAMINE, "", config.examineText(), null);
 		}
 	}
 
-	private void startSequence()
+	@Subscribe
+	public void onClientTick(ClientTick event)
 	{
-		closeReaperDialogue();
-
-		phase = Phase.WAIT_ANIM;
-		animSeen = false;
-		animStartCycle = client.getGameCycle();
-		dialogueOpenCycle = -1;
-
-		clientThread.invokeLater(() ->
+		if (redClickPoint != null
+			&& client.getGameCycle() - redClickStartCycle >= RED_CLICK_CYCLES)
 		{
-			Player p = client.getLocalPlayer();
-			if (p != null)
+			redClickPoint = null;
+			redClickStartCycle = -1;
+		}
+	}
+
+	private void startDeathSequence()
+	{
+		clientThread.invoke(() ->
+		{
+			removeDeathObject();
+
+			Player player = client.getLocalPlayer();
+			if (player == null)
 			{
-				p.setAnimation(config.animationId());
+				return;
 			}
+
+			LocalPoint spawnPoint = getDeathSpawnPoint(player);
+			Model deathModel = buildDeathModel();
+			if (spawnPoint == null || deathModel == null)
+			{
+				log.warn("Unable to construct Death sequence");
+				return;
+			}
+
+			RuneLiteObject death = client.createRuneLiteObject();
+			death.setModel(deathModel);
+			death.setLocation(spawnPoint, player.getWorldLocation().getPlane());
+			death.setOrientation(orientationToward(spawnPoint, player.getLocalLocation()));
+
+			AnimationController attack = new AnimationController(client, DEATH_ATTACK_ANIMATION)
+				.setOnFinished(controller ->
+				{
+					if (deathObject == death)
+					{
+						removeDeathObject();
+					}
+				});
+			death.setAnimationController(attack);
+
+			deathObject = death;
+			deathOverheadText = DEATH_OVERHEAD_TEXT;
+			death.setActive(true);
+
+			player.setAnimation(PLAYER_STUN_ANIMATION);
+			client.playSoundEffect(STUN_SOUND_EFFECT);
 		});
 	}
 
-	@Subscribe
-	public void onClientTick(ClientTick tick)
+	private void recordRedClick()
 	{
-		// If chatbox was closed (e.g. click), unregister listeners and clear reference
-		if (reaperDialogue != null && reaperDialogue.isClosed())
-		{
-			unregisterDialogueListeners();
-			reaperDialogue = null;
-			if (phase == Phase.DIALOGUE_OPEN)
-			{
-				phase = Phase.IDLE;
-			}
-		}
-
-		final Player p = client.getLocalPlayer();
-		if (p == null)
+		Point mouse = client.getMouseCanvasPosition();
+		if (mouse == null)
 		{
 			return;
 		}
 
-		if (phase == Phase.WAIT_ANIM)
+		redClickPoint = new Point(mouse.getX(), mouse.getY());
+		redClickStartCycle = client.getGameCycle();
+	}
+
+	private LocalPoint getDeathSpawnPoint(Player player)
+	{
+		WorldPoint playerWorld = player.getWorldLocation();
+		GameObject nearestPortal = null;
+		int nearestDistance = Integer.MAX_VALUE;
+
+		for (GameObject portal : portals)
 		{
-			final int anim = p.getAnimation();
-
-			if (anim == config.animationId())
+			if (portal == null || portal.getPlane() != playerWorld.getPlane())
 			{
-				animSeen = true;
-				return;
+				continue;
 			}
 
-			if (animSeen)
+			WorldPoint portalWorld = portal.getWorldLocation();
+			int distance = portalWorld.distanceTo2D(playerWorld);
+			if (distance < nearestDistance)
 			{
-				afterAnimation();
-				return;
+				nearestDistance = distance;
+				nearestPortal = portal;
 			}
+		}
 
-			if (client.getGameCycle() - animStartCycle > 60)
+		if (nearestPortal == null)
+		{
+			WorldPoint fallback = new WorldPoint(
+				playerWorld.getX(),
+				playerWorld.getY() + 1,
+				playerWorld.getPlane());
+			return LocalPoint.fromWorld(client, fallback);
+		}
+
+		WorldPoint portalWorld = nearestPortal.getWorldLocation();
+		int dx = Integer.compare(playerWorld.getX(), portalWorld.getX());
+		int dy = Integer.compare(playerWorld.getY(), portalWorld.getY());
+		if (dx == 0 && dy == 0)
+		{
+			dy = 1;
+		}
+
+		WorldPoint spawnWorld = new WorldPoint(
+			portalWorld.getX() + dx,
+			portalWorld.getY() + dy,
+			portalWorld.getPlane());
+
+		LocalPoint local = LocalPoint.fromWorld(client, spawnWorld);
+		return local != null ? local : nearestPortal.getLocalLocation();
+	}
+
+	private Model buildDeathModel()
+	{
+		NPCComposition composition = client.getNpcDefinition(HALLOWEEN_DEATH_NPC_ID);
+		if (composition == null || composition.getModels() == null)
+		{
+			return null;
+		}
+
+		int[] modelIds = composition.getModels();
+		ModelData[] parts = new ModelData[modelIds.length];
+		for (int i = 0; i < modelIds.length; i++)
+		{
+			parts[i] = client.loadModelData(modelIds[i]);
+			if (parts[i] == null)
 			{
-				afterAnimation();
+				return null;
 			}
+		}
+
+		ModelData modelData = client.mergeModels(parts);
+		if (modelData == null)
+		{
+			return null;
+		}
+
+		short[] colorsToReplace = composition.getColorToReplace();
+		short[] replacementColors = composition.getColorToReplaceWith();
+		if (colorsToReplace != null
+			&& replacementColors != null
+			&& colorsToReplace.length == replacementColors.length)
+		{
+			modelData = modelData.cloneColors();
+			for (int i = 0; i < colorsToReplace.length; i++)
+			{
+				modelData.recolor(colorsToReplace[i], replacementColors[i]);
+			}
+		}
+
+		return modelData.light();
+	}
+
+	private int orientationToward(LocalPoint from, LocalPoint to)
+	{
+		int dx = to.getX() - from.getX();
+		int dy = to.getY() - from.getY();
+		double radians = Math.atan2(-dx, -dy);
+		return ((int) Math.round(radians * 1024.0 / Math.PI)) & 2047;
+	}
+
+	private void refreshPortals()
+	{
+		portals.clear();
+
+		Scene scene = client.getScene();
+		if (scene == null || scene.getTiles() == null)
+		{
 			return;
 		}
 
-		// Open dialogue ONCE
-		if (phase == Phase.WAIT_DIALOGUE && dialogueOpenCycle != -1 && client.getGameCycle() >= dialogueOpenCycle)
+		Tile[][][] tiles = scene.getTiles();
+		for (Tile[][] plane : tiles)
 		{
-			openReaperDialogue();
-		}
-	}
-
-	@Subscribe
-	public void onGameTick(GameTick tick)
-	{
-		// Close on next tick after SPACE (dialogue sets closeRequested)
-		if (reaperDialogue != null && reaperDialogue.isCloseRequested())
-		{
-			closeReaperDialogue();
-			phase = Phase.IDLE;
-		}
-	}
-
-	private void afterAnimation()
-	{
-		phase = Phase.WAIT_DIALOGUE;
-
-		clientThread.invokeLater(() ->
-		{
-			final Player p = client.getLocalPlayer();
-			if (p == null)
+			if (plane == null)
 			{
-				phase = Phase.IDLE;
-				return;
+				continue;
 			}
 
-			p.createSpotAnim(SPOTANIM_KEY, config.spotAnimationId(), config.spotAnimationOffset(), config.spotAnimationDelay());
-			client.playSoundEffect(config.soundEffectId());
+			for (Tile[] row : plane)
+			{
+				if (row == null)
+				{
+					continue;
+				}
 
-			p.setOverheadText(config.overheadMessage());
-			p.setOverheadCycle(OVERHEAD_CYCLES);
-			client.addChatMessage(ChatMessageType.PUBLICCHAT, "", config.chatMessage(), client.getLocalPlayer().getName());
+				for (Tile tile : row)
+				{
+					if (tile == null)
+					{
+						continue;
+					}
 
-			dialogueOpenCycle = client.getGameCycle() + OVERHEAD_CYCLES;
-		});
+					for (GameObject object : tile.getGameObjects())
+					{
+						if (object != null && object.getId() == PORTAL_OBJECT_ID)
+						{
+							portals.add(object);
+						}
+					}
+				}
+			}
+		}
 	}
 
-	private void openReaperDialogue()
+	private void resetSequence()
 	{
-		if (reaperDialogue != null) // already open
-		{
-			return;
-		}
-
-		// mark as open so onClientTick doesn't reopen it forever
-		phase = Phase.DIALOGUE_OPEN;
-		dialogueOpenCycle = -1;
-
-		final int REAPER_NPC_ID = 9855;
-		final int CHATHEAD_SEQ = 563;
-
-		reaperDialogue = new GrimReaperDialogueInput(
-				clientThread,
-				chatboxPanelManager,
-				REAPER_NPC_ID,
-				CHATHEAD_SEQ,
-				"Grim Reaper",
-				"you're not welcome here"
-		);
-
-		chatboxPanelManager.openInput(reaperDialogue);
+		removeDeathObject();
+		redClickPoint = null;
+		redClickStartCycle = -1;
 	}
 
-
-	private void closeReaperDialogue()
+	private void removeDeathObject()
 	{
-		if (reaperDialogue != null)
+		if (deathObject != null && deathObject.isActive())
 		{
-			unregisterDialogueListeners();
-			reaperDialogue = null;
+			deathObject.setActive(false);
 		}
-		chatboxPanelManager.close();
+		deathObject = null;
+		deathOverheadText = null;
 	}
 
-	private void registerDialogueListeners()
+	private static boolean isEnterOption(String option)
 	{
-		if (reaperDialogue == null)
-		{
-			return;
-		}
-		keyManager.registerKeyListener(reaperDialogue);
-		mouseManager.registerMouseListener(reaperDialogue);
+		return option != null
+			&& (option.equalsIgnoreCase("Enter")
+			|| option.equalsIgnoreCase("Enter " + PORTAL_NAME));
 	}
 
-	private void unregisterDialogueListeners()
+	private static boolean isPortal(String option, String target)
 	{
-		if (reaperDialogue == null)
-		{
-			return;
-		}
-		keyManager.unregisterKeyListener(reaperDialogue);
-		mouseManager.unregisterMouseListener(reaperDialogue);
+		String portalName = PORTAL_NAME.toLowerCase();
+		return (target != null && target.toLowerCase().contains(portalName))
+			|| (option != null && option.toLowerCase().contains(portalName));
 	}
 }
